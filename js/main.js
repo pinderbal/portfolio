bfb973a1190a8fd1c2aa62aca7dcc3f3892b2adb
pinderbal <pbal7867@hotmail.com>
2020-10-06 13:56:41 -0400

    portfolio

---
+++ js/main.js
@@ -0,0 +1,26 @@
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+});
+
+sr.reveal('.jumbo-info',{});
+// sr.reveal('.avatar',{delay: 200});
+// sr.reveal('.display-4',{delay: 400});
+// sr.reveal('.display-test',{delay: 400});
+
+sr.reveal('.skills-title',{}); 
+sr.reveal('.skills-description',{delay: 200}); 
+sr.reveal('.skill-img',{ interval: 70});
+
+sr.reveal('.projects-title',{}); 
+sr.reveal('.projects-description',{delay: 200});
+sr.reveal('.project-item',{ interval: 100});
+
+
+sr.reveal('.contact_title',{}); 
+sr.reveal('.contact_description',{delay: 200});
+sr.reveal('.form-row',{ interval: 200});
+sr.reveal('.form-group', {interval: 200});
+sr.reveal('.form-btn',{delay: 200});
