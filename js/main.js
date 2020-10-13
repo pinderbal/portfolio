37629947010d800003e37c92a7e347bc363eb207
pinderbal <pbal7867@hotmail.com>
2020-10-13 19:52:21 -0400

    Added responsive mobile layout

--- js/main.js
+++ js/main.js
@@ -1,14 +1,12 @@
-const sr = ScrollReveal({
+window.sr = ScrollReveal({
     origin: 'top',
     distance: '100px',
     duration: 2000,
-    reset: true
+    reset: false,
+    mobile: false
 });
 
 sr.reveal('.jumbo-info',{});
-// sr.reveal('.avatar',{delay: 200});
-// sr.reveal('.display-4',{delay: 400});
-// sr.reveal('.display-test',{delay: 400});
 
 sr.reveal('.skills-title',{}); 
 sr.reveal('.skills-description',{delay: 200}); 
@@ -18,9 +16,9 @@ sr.reveal('.projects-title',{});
 sr.reveal('.projects-description',{delay: 200});
 sr.reveal('.project-item',{ interval: 100});
 
+sr.reveal('.contact-title',{}); 
+sr.reveal('.contact-description',{delay: 200}); 
 
-sr.reveal('.contact_title',{}); 
-sr.reveal('.contact_description',{delay: 200});
 sr.reveal('.form-row',{ interval: 200});
 sr.reveal('.form-group', {interval: 200});
 sr.reveal('.form-btn',{delay: 200});
